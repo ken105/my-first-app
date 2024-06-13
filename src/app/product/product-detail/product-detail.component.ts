6fa77e0aeb2e7bbb8abd9977b83a8e25b0db546d
ken105 <69455911+ken105@users.noreply.github.com>
2024-06-13 14:31:35 +0900

    フロントエンドとバックエンドの結合 (#2)

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {products} from '../../products'
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,15 +9,55 @@ import {products} from '../../products'
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
-export class ProductDetailComponent implements OnInit{
+export class ProductDetailComponent implements OnInit {
   product: any;
-  
-  constructor(private route: ActivatedRoute){}
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
+
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')!]
+      // this.product = this.productService.getProductById(params.get('productId')!)
+      // this.product = products[+params.get('productId')!]
+      const productObservable = this.productService.getProductById(params.get('productId')!)
+      productObservable.subscribe(
+        (data) => {
+          this.product = data
+        },
+        (err) => {
+
+        }
+      )
     })
+
+
+    // const observable = new Observable((subscriber) => {
+    //   subscriber.next(1);
+    //   subscriber.next(2);
+    //   subscriber.next(3);
+    //   setTimeout(() => {
+    //     subscriber.next(4);
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+    // debugger
+    // console.log('just before subscribe');
+    // debugger
+    // observable.subscribe({
+    //   next(x) {
+    //     console.log('got value ' + x);
+    //   },
+    //   error(err) {
+    //     console.error('something wrong occurred: ' + err);
+    //   },
+    //   complete() {
+    //     console.log('done');
+    //   },
+    // });
+    // console.log('just after subscribe');
+
   }
 
 }
